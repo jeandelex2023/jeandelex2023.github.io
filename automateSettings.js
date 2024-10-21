1f79795a0b0e142450ff63f8cfbdba0af198b11b
Jean Delex <jeandelex2@gmail.com>
2024-10-21 09:26:25 +0300

    Broweser controler test

--- automateSettings.js
+++ automateSettings.js
@@ -1,32 +1,44 @@
  // HOME PAGE SETTINGS
 document.getElementById('showMenu').addEventListener('click', function() {
   var sideDiv = document.getElementById('sideDiv');
-  if (sideDiv.style.right === '-660px') {
+  if (sideDiv.style.right === '-682px') {
     sideDiv.style.right = '0'; // Display div 0px from the right
   }
  else {
-    sideDiv.style.right = '-660px'; // Hide div off to the right
+    sideDiv.style.right = '-682px'; // Hide div off to the right
 
   }
 });
  // ABOUT PAGE SETTINGS
 document.getElementById('showMenuAbout').addEventListener('click', function() {
 var sideDiv2 = document.getElementById('sideDiv2');
-if (sideDiv2.style.right === '-660px'){
+if (sideDiv2.style.right === '-682px'){
   sideDiv2.style.right = '0'; 
 }
 else{
-  sideDiv2.style.right = '-660px';
+  sideDiv2.style.right = '-682px';
 }
 });
 
  // CV PAGE SETTINGS
  document.getElementById('showMenuMyCV').addEventListener('click', function() {
   var sideDiv3 = document.getElementById('sideDiv3');
-  if (sideDiv3.style.right === '-660px'){
+  if (sideDiv3.style.right === '-682px'){
     sideDiv3.style.right = '0'; 
   }
   else{
-    sideDiv3.style.right = '-660px';
+    sideDiv3.style.right = '-682px';
   }
   });
+
+   // WORKS PAGE SETTINGS
+ document.getElementById('showMenuWorks').addEventListener('click', function() {
+  var sideDiv4 = document.getElementById('sideDiv4');
+  if (sideDiv4.style.right === '-682px'){
+    sideDiv4.style.right = '0'; 
+  }
+  else{
+    sideDiv4.style.right = '-682px';
+  }
+  });
+// SLIDE SETTINGS
